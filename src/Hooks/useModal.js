f9ee27066a701c6b9cc305d17451ded26c8e5f61
https://github.com:kristian-second-account <kristian@kmeier.dk>
2022-12-12 08:50:42 +0100

    fix useModal

--- src/Hooks/useModal.js
+++ src/Hooks/useModal.js
@@ -3,11 +3,7 @@ import { useState } from 'react'
 export const useModal = () => {
   const [isModalVisible, setIsModalVisible] = useState(false)
 
-  function toggleModal() {
-    setIsModalVisible(!isModalVisible)
-  }
-  return {
-    isModalVisible,
-    toggleModal,
-  }
+  const toggleModal = () => setIsModalVisible(!isModalVisible)
+
+  return { isModalVisible, toggleModal }
 }
